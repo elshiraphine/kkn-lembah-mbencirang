7bf2cc8d140445c701f310c6eb7b6c52d7be987e
SRYian <92420947+SRYian@users.noreply.github.com>
2022-10-04 20:56:37 +0700

    feat: add 3D prefab, add new build setting
    Added and fixed a new build settings for the template phones

---
+++ Assets/Scripts/Collision_AR.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_AR : MonoBehaviour
+{
+    public GameObject secondBody;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hello");
+    }
+    public void EnableSecond()
+    {
+        Debug.Log("Hello");
+
+        secondBody.SetActive(false);
+    }
+}
