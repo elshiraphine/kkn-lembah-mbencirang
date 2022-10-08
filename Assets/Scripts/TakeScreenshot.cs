777454d8a9fcf1891f0a71fb7dffbb999697d4bf
SRYian <92420947+SRYian@users.noreply.github.com>
2022-10-09 02:58:48 +0700

    feat: add new models, screenshot mode

---
+++ Assets/Scripts/TakeScreenshot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeScreenshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] disabledUI;
+
+    public void TakeSC()
+    {
+        foreach (GameObject item in disabledUI)
+        {
+            item.SetActive(false);
+        }
+        StartCoroutine(Capture());
+    }
+
+    IEnumerator Capture()
+    {
+        string timestamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        string fileName = "screenshot" + timestamp + ".png";
+        string savePath = fileName;
+        ScreenCapture.CaptureScreenshot(savePath);
+        yield return new WaitForEndOfFrame();
+        foreach (GameObject item in disabledUI)
+        {
+            item.SetActive(true);
+        }
+    }
+}
